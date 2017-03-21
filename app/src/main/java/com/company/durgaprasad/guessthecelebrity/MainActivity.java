6642f0010422f8dd88e9eedf1d39d06db4f08327
Durgaprasad Nagarkatte <durgaprasad.nagarkatte@gmail.com>
2017-03-21 20:28:33 +0530

    First Commit

---
+++ app/src/main/java/com/company/durgaprasad/guessthecelebrity/MainActivity.java
@@ -0,0 +1,177 @@
+package com.company.durgaprasad.guessthecelebrity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static android.R.attr.animationDuration;
+import static android.R.attr.button;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> celebURLs = new ArrayList<>();
+    ArrayList<String> celebNames = new ArrayList<>();
+    int chosenCeleb = 0;
+    ImageView imageView;
+    int locationOfCorrectAnswer = 0;
+    String[] answers = new String[4];
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+
+    public class ImageDownloder extends AsyncTask<String, Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            URL url;
+            HttpURLConnection urlConnection = null;
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection)url.openConnection();
+                urlConnection.connect();
+                InputStream inputStream = urlConnection.getInputStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                return bitmap;
+            }
+            catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection)url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+                while(data != -1){
+                    char current  = (char)data;
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+            }
+            catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        String result = "";
+        DownloadTask task = new DownloadTask();
+        try {
+            result = task.execute("http://www.posh24.se/kandisar/").get();
+            String[] splitResult = result.split("<div class=\"sidebarContainer\">");
+            Pattern p = Pattern.compile("<img src=\"(.*?)\"");
+            Matcher m = p.matcher(splitResult[0]);
+            while (m.find()) {
+
+                celebURLs.add(m.group(1));
+                System.out.println(m.group(1));
+
+            }
+
+            p = Pattern.compile("alt=\"(.*?)\"");
+            m = p.matcher(splitResult[0]);
+
+            while (m.find()) {
+
+                celebNames.add(m.group(1));
+                System.out.println(m.group(1));
+
+            }
+
+            Random random = new Random();
+            chosenCeleb = random.nextInt(celebURLs.size());
+            ImageDownloder imageTask = new ImageDownloder();
+            Bitmap celebImage = imageTask.execute(celebURLs.get(chosenCeleb)).get();
+            imageView.setImageBitmap(celebImage);
+
+            locationOfCorrectAnswer = random.nextInt(4);
+            int inCorrectAnswer;
+
+            //for loop run for generating 3 random numbers with the correct answer
+            for(int i=0; i < 4; i++){
+                if(i == locationOfCorrectAnswer){
+                    answers[i] = celebNames.get(chosenCeleb);
+                }
+                else{
+                    inCorrectAnswer = random.nextInt(celebNames.size());
+
+                    //T0 avoid from getting the correct answer as the random number
+                    while(inCorrectAnswer == chosenCeleb){
+                        inCorrectAnswer = random.nextInt(41);
+                    }
+                    answers[i] = celebNames.get(inCorrectAnswer);
+                }
+            }
+            button1.setText(answers[0]);
+            button2.setText(answers[1]);
+            button3.setText(answers[2]);
+            button4.setText(answers[3]);
+
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void celebChosen(View view){
+        if(view.getTag().toString().equals(Integer.toString(locationOfCorrectAnswer))){
+            Toast.makeText(getApplicationContext(), "Correct Answer", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Wrong Answer! " + celebNames.get(chosenCeleb), Toast.LENGTH_SHORT).show();
+        }
+    }
+}
